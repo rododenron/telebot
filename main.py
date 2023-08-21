ab47183d9bb42b8be110b3a967732cea66a581d8
mike yak <yak@mail.ru>
2023-08-21 19:16:57 +0300

    Created config file. Modified main.py.

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-"""
-telegram bot
-"""
-
 import telebot
+import configparser
 
-TOKEN = '6197751387:AAF0qQN7ldQ61UeOPHHAgBJyKf-M3kJxwcg'
+from extensions import ConversionException, APILayerException, APILayer, Utils
 
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+token = config.get('secrets', 'token')
+apikey = config.get('secrets', 'apikey')
+
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = ('Chtoby nachat rabotu vvedite comandu v sleduyushem formate: \n<Imya valyuty> <v kakuyu valutu perevesti>'
+            '\\ <kolichestvo perevodimoy valuty>')
+
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    currencies = APILayer.get_values(apikey=apikey)
+    currencies_list = telebot.util.split_string(str(currencies), 3000)
+    for part in currencies_list:
+        bot.reply_to(message, part)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    message_text = message.text
+    try:
+        Utils.check_input(message_text, APILayer.get_values(apikey=apikey))
+
+        currency_from, currency_to, amount = message.text.split(' ')
+        text = APILayer.get_price(currency_from, currency_to, amount, apikey)
+        bot.reply_to(message, text)
+    except ConversionException as e:
+        bot.reply_to(message, e)
+    except APILayerException as e:
+        bot.reply_to(message, e)
+    except Exception as e:
+        bot.reply_to(message, e)
+
+bot.polling()
