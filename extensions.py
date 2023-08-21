5a4bc59fdbc99ff5b65de1ff5a9558b0b3a6c711
mike yak <yak@mail.ru>
2023-08-21 19:17:57 +0300

    PArt of code dedicated to file extensions.py

---
+++ extensions.py
@@ -0,0 +1,58 @@
+import requests
+import json
+
+
+class ConversionException(BaseException):
+    def __int__(self):
+        pass
+
+
+class APILayerException(BaseException):
+    def __int__(self):
+        pass
+
+
+class APILayer:
+    @staticmethod
+    def get_price(currency_to: str, currency_from: str, amount: str, apikey: str):
+        ans = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to={currency_to}&from={currency_from}&amount={amount}&apikey={apikey}")
+        ans_json = json.loads(ans.content)
+        if ans_json.get('success', False):
+            text = f"Price {amount} {currency_from} in {currency_to} is {ans_json['result']}"
+        else:
+            raise APILayerException(f"{ans_json['message']}")
+        return text
+
+    @staticmethod
+    def get_values(apikey: str):
+        ans = requests.get(f"https://api.apilayer.com/exchangerates_data/symbols?apikey={apikey}")
+        ans_json = json.loads(ans.content)
+        if ans_json.get('success', False):
+            text = "\n".join([f"{key}: {value}" for key, value in ans_json['symbols'].items()])
+        else:
+            raise APILayerException(f"{ans_json['message']}")
+        return text
+
+
+class Utils:
+    @staticmethod
+    def check_input(input_string: str, template):
+
+        if len(input_string.split(' ')) != 3:
+            raise ConversionException('Ne vernoe kolvo vhodny dannyh')
+
+        currency_from, currency_to, amount = input_string.split(' ')
+
+        if currency_from == currency_to:
+            raise ConversionException(f'Nevozmozhno perevesti odinakovie valuty {currency_to}')
+
+        if not amount.isnumeric():
+            raise ConversionException(f'Nevozmozhno konvertiorovat velichinu {amount}')
+
+        if not (currency_to in template):
+            raise ConversionException(f'Valuta ne nayadena{currency_to}')
+
+        if not (currency_from in template):
+            raise ConversionException(f'Valuta ne nayadena{currency_from}')
+
+        return currency_from, currency_to, amount
